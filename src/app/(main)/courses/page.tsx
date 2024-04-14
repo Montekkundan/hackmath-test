9733b599ec75b2933e1bfdf7c7a57da50f1f0cba
Montek <87750128+Montekkundan@users.noreply.github.com>
2024-04-13 17:14:27 -0700

    courses page now shows option for ai courses

--- src/app/(main)/courses/page.tsx
+++ src/app/(main)/courses/page.tsx
@@ -1,6 +1,8 @@
 import { getCourses, getUserData } from "@/db/queries";
 
 import { List } from "./list";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const CoursesPage = async () => {
   const coursesData = getCourses();
@@ -16,6 +18,18 @@ const CoursesPage = async () => {
 
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
+      <h1 className="text-2xl font-bold text-neutral-700">
+        Dr Ham  Recommneded Courses
+      </h1>
+      <div className="flex flex-col gap-6 items-center justify-center">
+      <span>To have Dr Ham Recommneded courses you need to take a knowledge test.</span>
+      <Button>
+        <Link href="/kc">
+        Take a knowledge test
+        </Link>
+        </Button>
+      </div>
+      
       <h1 className="text-2xl font-bold text-neutral-700">
         Math Courses
       </h1>
